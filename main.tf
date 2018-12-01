86b2581202a21837678e1b6bfea313148cb3f36c
Michelle Ly <lymichelle1@gmail.com>
2018-12-01 12:07:13 -0500

    added terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,5 @@
 # Create a new instance of the latest Ubuntu 14.04 on an
 # t2.micro node with an AWS Tag naming it "HelloWorld"
-provider "aws" {
-  region = "us-west-2"
-}
 
 data "aws_ami" "ubuntu" {
   most_recent = true
